0e0d924a8e3967ee52780f4f20b606a930d305ea
fredupstair <federico.porceddu@outlook.com>
2026-03-05 14:07:32 +0100

    Add new image assets for team visuals
    - Added outline and color PNG images for team e92ab8b1-14a2-4b3a-990d-523d03fa309c.
    - Added outline and color PNG images for team effb0076-cceb-40d9-b087-04c455ff7b80.

---
+++ fe/src/webparts/cepDashboard/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ICepDashboardWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'CepDashboardWebPartStrings' {
+  const strings: ICepDashboardWebPartStrings;
+  export = strings;
+}
